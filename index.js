0d25a1a61672f9d571ac4c79ea82c81b094a0172
Ange290 <uange209@gmail.com>
2024-02-20 11:40:06 +0200

    Creating index.js file, and updating ReadMe

---
+++ index.js
@@ -0,0 +1,21 @@
+library = [{
+    title :'The developer',
+    author:'Ange Lyka',
+    availability :true,
+},
+{
+    title :'Long Eagle',
+    author:'Anonymous',
+    availability :false,
+},
+{
+    title :'The Davinci Code',
+    author:'Dan Brown',
+    availability :true,
+},
+{
+    title :'Atomic Habits',
+    author:'James Clair',
+    availability :false,
+},
+]
